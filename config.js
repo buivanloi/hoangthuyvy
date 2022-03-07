b2dc61e6b1cae436084d10762c17e90e9d49b19a
buivanloi <100933683+buivanloi@users.noreply.github.com>
2022-03-07 16:20:22 +0700

    Add files via upload
    vy

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = '8/3';
+const giftUrl = 'https://www.facebook.com/htthuyvy1709';
+const eventName = 'Bùi Lợi chúc mừng ngày 8-3';
+const titleCard = 'Tặng Vy';
+const contentCard = 'Mới tiếp xúc với Vy một thời gian chưa đủ lâu nên không biết chúc gì cả. Thôi thì nhân ngày 8/3 chúc cô gái dễ thương nhất nhóm tràn ngập niềm vui và mãi hạnh phúc bên người mình yêu nhé! Ký tên: Lợi ngu ngok';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'thuyvy.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
